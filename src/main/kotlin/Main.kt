a8a4453ccb93a02d1e0d5bde96305a19cb58ca76
vbird <venetiabird@gmail.com>
2024-05-03 16:04:21 +1000

    chore: exercise 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,10 @@ package org.example
 
 fun main() {
     println("Hello World!")
+    println(Lizt.of(arrayOf(1, 2, 3)))
+//    Liz.sum(Lizt.of(3,4,5))
 
+    println(Lizt.product(Lizt.of(1.0, 2.0)))
 }
 
 
---
+++ src/main/kotlin/fpList.kt
@@ -0,0 +1,29 @@
+package org.example
+
+
+sealed class Lizt<out A> {
+    companion object {
+        fun <A> of(vararg aa: A): Lizt<A> {
+            val tail = aa.sliceArray(1 until aa.size)
+            return if (aa.isEmpty()) Nil else Cons(aa[0], of(*tail))
+        }
+
+        fun sum(int: Lizt<Int>): Int =
+            when (int) {
+                is Nil -> 0
+                is Cons -> int.head + sum(int.tail)
+            }
+        fun product(doubles: Lizt<Double>): Double =
+            when (doubles) {
+                is Nil -> 1.0
+                is Cons ->
+                    if (doubles.head == 0.0) 0.0
+                    else
+                        doubles.head * product(doubles.tail)
+            }
+
+    }
+}
+object Nil : Lizt<Nothing>()
+data class Cons<out A>(val head: A, val tail: Lizt<A>) : Lizt<A>()
+
